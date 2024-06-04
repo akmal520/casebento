fc0944ac0187ddf93479f3f86f30078fc7b8bc29
akmal520 <akmalbentar15@gmail.com>
2024-06-04 21:56:37 +0700

    feat: add dashboard page, thank-you page after payment the product and send an email to costumer when a product is shipped

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -16,3 +17,34 @@ export const kFormatter = (num: any) => {
         ? Math.sign(num) * parseFloat((Math.abs(num) / 1000).toFixed(1)) + 'K'
         : Math.sign(num) * Math.abs(num);
 };
+
+export function constructMetadata({
+    title = 'CaseBento - custom high-quality phone cases',
+    description = 'Create custom high-quality phone cases in seconds',
+    image = '/fck-thumbnail.png',
+    icons = '/favicon.ico',
+}: {
+    title?: string;
+    description?: string;
+    image?: string;
+    icons?: string;
+} = {}): Metadata {
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            images: [{ url: image }],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            images: [image],
+            creator: '@casebento',
+        },
+        icons,
+        metadataBase: new URL('https://casebento.vercel.app'),
+    };
+}
